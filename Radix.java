5490afeb1177ef185f280b10e98f13993934536c
VictoriaSzy <vszymanowski00@stuy.edu>
2019-04-02 11:20:11 -0400

    Think I finished helper method to find maxDigits that I will have to work with and check for

--- Radix.java
+++ Radix.java
@@ -2,9 +2,15 @@ import java.util.* ;
 import java.io.* ;
 
 public class Radix {
+  public static void main(String[] args) {
+    int[] a = {1, 3, 5, 83, 256, 7226, 1, 6263622} ;
+    System.out.println(Arrays.toString(a)) ;
+    System.out.println("Max # of digits expected is 7 and we got: " + (int) maxDigits(a)) ;
+  }
+  @SuppressWarnings("unchecked")
   public static void radixsort(int[]data) {
-    MyLinkedList<Integer> buckets = new MyLinkedList<Integer>() ;
-    int maxNumOfDigits = maxDigits(data) ;
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10] ;
+    int maxNumOfDigits = (int) maxDigits(data) ;
     for (int i : data) {
       // thinking of storing it in either an array or MyLinkedList
     }
@@ -14,7 +20,14 @@ public class Radix {
       }
     }
   }
-  public static int maxDigits(int[] d) {
-    return 0 ;
+  public static double maxDigits(int[] d) {
+    double max = 0 ;
+    for (int i : d) {
+      if (Math.log(i) / 2 > max) {
+        // i has a greater number of digits than max
+        max = Math.log(i) / 2 ;
+      }
+    }
+    return max ;
   }
 }
