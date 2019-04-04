fb787ef3d7a4c8fdd996c0d07a1495eb05988b81
VictoriaSzy <vszymanowski00@stuy.edu>
2019-04-03 20:47:12 -0400

    Tried implementing and using iterator but got confused and debugged

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,70 +1,9 @@
 import java.io.* ;
 import java.util.* ;
-
-public class MyLinkedList<E> implements Iterable<E>{
-  public static void main(String[] args) {
-    System.out.println("Testing creating a MyLinkedList!") ;
-    MyLinkedList<Integer> a = new MyLinkedList<Integer>() ;
-    System.out.println(a.toString()) ;
-    if (a.size() == 0) System.out.println("The size should be 0 and it is!") ;
-    else {
-      System.out.println("There is an error in either creating the MyLinkedList or in retrieving the size!") ;
-    }
-    int[] d = {1, 2, 3, 4, 5, 11, 111, 53, 6} ;
-    for (int i = 0 ; i < 9 ; i++) {
-      System.out.println("Let's add " + d[i] + " to a.") ;
-      a.add(d[i]) ;
-    }
-    System.out.println("After adding here is how it looks: \n" + a.toString()) ;
-  }
-  //////// Start of Node class ////////////////////////////////
-  public class Node {
-    private E data ;
-    private Node next, prev ;
-    // Constructors
-    public Node(E d, Node p, Node n) {
-      data = d ;
-      next = n ;
-      prev = p ;
-    }
-    public Node(E d) {
-      data = d ;
-      prev = null ;
-      next = null ;
-    }
-    // accessors
-    public E getData() {
-      return data ;
-    }
-    public Node next() {
-      return next ;
-    }
-    public Node prev() {
-      return prev ;
-    }
-    public boolean hasNext() {
-      if (next == null) return false ;
-      return true ;
-    }
-    // mutators
-    public E setData(E d) {
-      data = d ;
-      return data ;
-    }
-    public void setNext(Node n) {
-      next = n ;
-    }
-    public void setPrev(Node p) {
-      prev = p ;
-    }
-    public String toString() {
-      String fin = "" ;
-      return fin ;
-    }
-  }
-  ///////////// END OF NODE CLASS ////////////////////////////////
+@SuppressWarnings("unchecked")
+public class MyLinkedList<E> {
   ///////////// START OF ITERATOR CLASS ////////////////
-  public class Iter implements Iterator<E> {
+  /*public class Iter implements Iterator<E> {
         Node current = null ;
         public Iter(MyLinkedList<E> a) {
             current = a.start ;
@@ -78,7 +17,7 @@ public class MyLinkedList<E> implements Iterable<E>{
             current = current.next ;
             return a ;
         }
-    }
+    }*/
     /////////// END OF ITERATOR CLASS //////////////////////////
   ////////// START OF MYLINKEDLIST CLASS METHODS /////////////////
   private int length ;
@@ -89,7 +28,7 @@ public class MyLinkedList<E> implements Iterable<E>{
     start = new Node(null, null, null) ;
     end = new Node(null, null, null) ;
   }
-  public MyLinkedList(E[] a) {
+  public MyLinkedList(E[] a) {       
     	length = a.length ;
     	if (a.length > 0){
     		start = new Node(a[0]) ;
@@ -101,9 +40,12 @@ public class MyLinkedList<E> implements Iterable<E>{
     		end = current ;
     	}
     }
-  public Iterator<E> iterator() {
-    return new Iter(this) ;
-  }
+    public E iterator(Node<E> node){
+      return node.next().getData() ;
+    }
+  //public Iterator<E> iterator() {
+    //return new Iter(this) ; // from 4/03/19 class notes
+  //}
   // returns size
   public int size() {
     return length ;
@@ -113,161 +55,45 @@ public class MyLinkedList<E> implements Iterable<E>{
         //in O(1) runtime (CONSTANT), move the elements from other onto the end of this
         //The size of other is reduced to 0
         //The size of this is now the combined sizes of both original lists
-        this.end.setNext(other.start) ;
-        this.end = other.end ;
-        other.start = null ;
-        other.end = null ;
-        this.length += other.length ;
-        other.length = 0 ;
+        if (size() == 0) {
+          start = other.start ;
+          end = other.end ; // sort of like transferring ownership
+        }
+        else if (other.size() > 0) {
+          end.setNext(other.start) ;
+          other.start.setPrev(this.end) ;
+          end = other.end ;
+        }
+        length += other.size() ;
+        other.clear() ;
     }
-
-  // returns the Node at the specified index
-  private Node getNthNode(int n) {
-    if (n < 0 || n >= length) throw new IndexOutOfBoundsException("The index given is not valid and can't be used to find a Node!") ;
-    Node result = start ;
-    for (int i = 0 ; i < length && i < n ; i++) {
-      // I'm using i as a visual representation of the index that an array would have
-      result = result.next() ;
-    }
-    return result ;
-  }
-
-  // returns the Integer at the specified index
-  public E get(int index) {
-    if (index < 0 || index >= length) throw new IndexOutOfBoundsException("The index given is not valid!") ;
-    Node item = getNthNode(index) ;
-    return item.getData() ;
-  }
-
-  // changes the Integer at the specified index and returns the old or previous value there
-  public E set(int index, E value) {
-    if (index < 0 || index >= length) throw new IndexOutOfBoundsException("The index given is not valid!") ;
-    Node result = start ;
-    for (int i = 0 ; i < length ; i++) {
-      result = result.next() ;
-    }
-    E previousData = result.getData() ;
-    result.setData(value) ;
-    return result.getData() ;
-  }
-
-  // returns whether the Linked List has the given value
-  public boolean contains(E value) {
-    Node result = start ;
-    for (int i = 0 ; i < length ; i++) {
-      if (result.getData() == value) return true ;
-      result = result.next() ;
-    }
-    return false ;
-  }
   public void clear() {
     length = 0 ;
 		start = null ;
     end = null ;
 	}
-  // returns the index of the value
-  // if the value is not in the Linked List --> -1
-  public int indexOf(E value) {
-    Node current = start ;
-    int index = 0 ;
-    while (current != null) {
-      // used the while loop from the website
-      E v = current.getData() ;
-      if (v.equals(value)) return index ;
-      current = current.next() ;
-      index++ ;
-    }
-    // if the value is not in the LinkedList, return -1
-    return -1 ;
-  }
-
   // returns true if the Integer was added successfully
   // and adds it to the end
   public boolean add(E value) {
     if (length == 0) {
+      Node<E> v = new Node<E>(value, null, null) ;
+      start = v ;
+      end = v ;
+      length ++ ;
       // if the linked list doesn't have anything yet
-      end = new Node(value) ;
-      start = end ;
-      length++ ;
     }
     else {
       // if the linked list has something in it, then we have to add on to the last node
-      Node nextNode = new Node(value, end, null) ;
+      Node<E> nextNode = new Node<E>(value, end, null) ;
       end.setNext(nextNode) ;
-      end = end.next() ;
+      end = nextNode ;
       length++ ;
     }
     return true ;
   }
-
-  // adds an Integer to a specific index (as long as it is valid)
-  public void add(int index, E value) {
-    if (index < 0 || index > length) throw new IndexOutOfBoundsException("The index given does not exist. Therefore, nothing can be changed!") ;
-    Node nodeToAdd = new Node(value, null, null) ;
-    if (index == 0) {
-      // we are adding the Integer to the front
-      start.setPrev(nodeToAdd) ;
-      nodeToAdd.setNext(start) ;
-      start = nodeToAdd ;
-      length++ ;
-    }
-    else if (index == length) {
-      // add element to the end
-      add(value) ;
-    }
-    else {
-      // we are adding the Integer somewhere into the list --> we need to shift the Integer at the index
-      Node nodeToBeShifted = getNthNode(index) ;
-      nodeToAdd.setPrev(nodeToBeShifted.prev()) ;
-      nodeToAdd.setNext(nodeToBeShifted) ;
-      Node nodeThatStays = nodeToBeShifted.prev() ;
-      nodeThatStays.setNext(nodeToAdd) ;
-      nodeToBeShifted.setPrev(nodeToAdd) ;
-      length++ ;
-    }
+  public Node<E> start() {
+    return start ;
   }
-
-  // removes the Integer at the given index and returns what was removed
-  public E remove(int index) {
-    if (index < 0 || index >= length) throw new IndexOutOfBoundsException("The given index can't be used!") ;
-    E old = get(index) ;
-    if (index == 0) {
-      // we are getting rid of the first Integer
-      start = start.next() ;
-      start.setPrev(null) ;
-      length-- ;
-      return old ;
-    }
-    if (index == length - 1) {
-      // this means that we will be removing the last Integer
-      end = end.prev() ;
-      end.setNext(null) ;
-      length-- ;
-      return old ;
-    }
-    // otherwise, we need to remove an Integer that is not the first or last
-    Node prevNode = getNthNode(index - 1) ;
-    Node nextNode = getNthNode(index + 1) ;
-    prevNode.setNext(nextNode) ;
-    nextNode.setPrev(prevNode) ;
-    length-- ;
-    return old ;
-  }
-
-  // returns true if the value exists in the Linked List
-  // returns false if the value is NOT in the Linked List
-  public boolean remove(E value) {
-    int index = indexOf(value) ;
-    if (index == -1) return false ; // since the Integer is not in the Linked List
-    else {
-      remove(index) ;
-      return true ;
-    }
-  }
-  public E removeFront() {
-    return remove(0) ;
-  }
-
   // prints out the Linked List with the node(s)
   public String toString() {
     String fin = "[" ;
